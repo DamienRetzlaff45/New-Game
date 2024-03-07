d5ae677f2b87b8c23086705d5fc5a5268d05c5dc
JadenDaSuperSenior05 <161770443+JadenDaSuperSenior05@users.noreply.github.com>
2024-03-07 14:29:41 -0700

    start

---
+++ script.js
@@ -0,0 +1,9 @@
+let magicPower = 0;
+let clickPower = 0;
+let autoClickPower = 0;
+
+document.getElementById('start').addEventListener('click', function () {
+    document.getElementById('description').style.visibility = 'hidden';
+    document.getElementById('start').style.visibility = 'hidden';
+    document.getElementById('object').style.visibility = 'visible';
+})
